d6f075547d116b86c45ee4efd95ae2e7fd04c8b3
manchan <67535496+manvith12@users.noreply.github.com>
2024-10-18 11:12:01 +0530

    yes

---
+++ oddoreven.c
@@ -0,0 +1,15 @@
+//C Program to Check Whether Number is Even or odd
+#include <stdio.h>
+int main()
+{
+    int a
+    scanf("%d",&a);
+    if(a%2==0)
+    {
+        printf("Even");
+    }
+    else
+    {
+        printf("Odd");
+    }
+}
